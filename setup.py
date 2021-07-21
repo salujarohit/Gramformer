a54814797079fa92642424ee282a7afd8e05f20d
Rohit Saluja <salujarohit@outlook.com>
2021-07-21 17:55:56 +0300

    Added updated transformers

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     long_description="A framework for detecting, highlighting and correcting grammatical errors on natural language text",
     url="https://github.com/PrithivirajDamodaran/Gramformer.git",
     packages=setuptools.find_packages(),
-    install_requires=['transformers', 'sentencepiece==0.1.95', 'python-Levenshtein==0.12.2', 'fuzzywuzzy==0.18.0',  'tokenizers', 'fsspec==2021.5.0', 'lm-scorer', 'errant'],
+    install_requires=['transformers @ git+https://github.com/huggingface/transformers@v4.8.2#egg=transformers', 'sentencepiece==0.1.95', 'python-Levenshtein==0.12.2', 'fuzzywuzzy==0.18.0',  'tokenizers', 'fsspec==2021.5.0', 'lm-scorer', 'errant'],
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "License :: Apache 2.0",
